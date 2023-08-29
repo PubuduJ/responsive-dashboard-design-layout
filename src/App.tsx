75c7b3ee40eb490e6d47c5fbc49387670b0d761a
Pubudu Janith <pubudujanith123@gmail.com>
2023-08-29 16:26:41 +0530

    Implement main-layout logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
 
 const App = () => {
   return (
-      <>
+      <BrowserRouter>
+          <Routes>
+              <Route path={"/"} element={<MainLayout/>}>
 
-      </>
+              </Route>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,7 +1,38 @@
+import {Box, Toolbar} from "@mui/material";
+import TopBar from "../common/TopBar";
+import sizeConfigs from "../../configs/sizeConfigs";
+import SideBar from "../common/SideBar";
+import colorConfigs from "../../configs/colorConfigs";
+import {Outlet} from "react-router-dom";
+
 const MainLayout = () => {
     return (
         <>
-
+            <Box sx={{display: "flex"}}>
+                <TopBar/>
+                <Box
+                    component={"nav"}
+                    sx={{
+                        width: sizeConfigs.sidebar.width,
+                        flexShrink: 0
+                    }}
+                >
+                    <SideBar/>
+                </Box>
+                <Box
+                    component={"main"}
+                    sx={{
+                        flexGrow: 1,
+                        p: 3,
+                        width: `calc(100% - ${sizeConfigs.sidebar.width})`,
+                        minHeight: "100vh",
+                        backgroundColor: colorConfigs.mainBg
+                    }}
+                >
+                    <Toolbar/>
+                    <Outlet/>
+                </Box>
+            </Box>
         </>
     )
 }

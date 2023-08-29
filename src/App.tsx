7ff6f35bf6d7cee893166d7db0760967aeb1604b
Pubudu Janith <pubudujanith123@gmail.com>
2023-08-29 16:38:02 +0530

    Update index.html file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
+import HomePage from "./pages/home/HomePage";
 
 const App = () => {
   return (
       <BrowserRouter>
           <Routes>
               <Route path={"/"} element={<MainLayout/>}>
-
+                  <Route index element={<HomePage/>}/>
               </Route>
           </Routes>
       </BrowserRouter>

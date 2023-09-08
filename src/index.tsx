0b71b4524b4ea5fc45349f9e4427813e3f79c73c
Pubudu Janith <pubudujanith123@gmail.com>
2023-09-08 13:11:17 +0530

    Update imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import {CssBaseline} from "@mui/material";
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
